e69be7b5da96287001f6540b3548f6d5dcb7a032
Ratilife <ratilife81@gmail.com>
2024-12-10 16:15:21 +0300

    Изучение технологии ANTLR4

--- ex6.py
+++ ex6.py
@@ -3,38 +3,61 @@ from PyQt5.QtGui import QSyntaxHighlighter, QTextCharFormat, QColor, QFont
 from PyQt5.QtCore import Qt
 from Python3.PythonLexer import PythonLexer
 
+# Класс PythonSyntaxHighlighter отвечает за подсветку синтаксиса для Python-кода
 class PythonSyntaxHighlighter(QSyntaxHighlighter):
     def __init__(self, document):
+        """
+        Инициализация объекта подсветки синтаксиса для документа.
+        
+        Этот метод создает словарь стилей для различных типов синтаксиса (ключевые слова, строки, комментарии и т.д.)
+        и вызывает инициализацию родительского класса QSyntaxHighlighter.
+
+        :param document: QTextDocument, документ, в котором будет применяться подсветка
+        """
         super().__init__(document)
         self.styles = {
-            'keyword': self.create_format('blue', 'bold'),
-            'string': self.create_format('green'),
-            'comment': self.create_format('gray', 'italic'),
-            'number': self.create_format('orange'),
-            'variable': self.create_format('purple'),
-            'default': self.create_format('black'),
+            'keyword': self.create_format('blue', 'bold'),      # Синий цвет и полужирное начертание для ключевых слов
+            'string': self.create_format('green'),              # Зеленый цвет для строковых литералов
+            'comment': self.create_format('gray', 'italic'),    # Серый цвет и курсив для комментариев
+            'number': self.create_format('orange'),             # Оранжевый цвет для числовых значений
+            'variable': self.create_format('purple'),           # Фиолетовый цвет для переменных
+            'default': self.create_format('black'),             # Черный цвет по умолчанию для остальных токенов
         }
 
     def create_format(self, color, style=''):
+        """
+        Создает формат для подсветки текста в зависимости от заданных стилей.
+
+        :param color: Цвет текста (например, 'blue', 'green' и т.д.)
+        :param style: Стиль текста (например, 'bold' или 'italic')
+        :return: QTextCharFormat объект, который хранит информацию о формате текста
+        """
         fmt = QTextCharFormat()
-        fmt.setForeground(QColor(color))
+        fmt.setForeground(QColor(color))                                    # Устанавливаем цвет текста
         if 'bold' in style:
-            fmt.setFontWeight(QFont.Bold)
+            fmt.setFontWeight(QFont.Bold)                                   # Если задан стиль 'bold', делаем текст полужирным
         if 'italic' in style:
-            fmt.setFontItalic(True)
+            fmt.setFontItalic(True)                                         # Если задан стиль 'italic', делаем текст курсивом
         return fmt
 
     def highlightBlock(self, text):
-        lexer = PythonLexer(InputStream(text))
-        token = lexer.nextToken()
+        """
+        Метод, который вызывается для каждого блока текста в документе.
+        Этот метод анализирует текст с помощью лексера PythonLexer и применяет соответствующий стиль
+        в зависимости от типа токена (ключевое слово, строка, комментарий и т.д.).
+
+        :param text: Строка текста, которую нужно подсветить
+        """
+        lexer = PythonLexer(InputStream(text))          # Инициализируем лексер для анализа текста
+        token = lexer.nextToken()                       # Получаем первый токен
 
         # Ключевые слова Python
         keywords = {"def", "class", "if", "else", "elif", "for", "while", "import", "from", "return", "print"}
 
-        while token.type != Token.EOF:
-            token_text = token.text
-            start_index = token.start
-            length = len(token_text)
+        while token.type != Token.EOF:                  # Пока не достигнут конец текста
+            token_text = token.text                     # Текст текущего токена
+            start_index = token.start                   # Начальная позиция токена
+            length = len(token_text)                    # Длина токена
 
             # Подсветка для ключевых слов
             if token_text in keywords:
@@ -53,7 +76,7 @@ class PythonSyntaxHighlighter(QSyntaxHighlighter):
                 self.setFormat(start_index, length, self.styles['number'])
 
             # Подсветка для переменных (идентификаторов)
-            elif token.type == PythonLexer.ID:
+            elif token.type == PythonLexer.NAME:  # Используйте NAME для идентификаторов
                 if token_text not in keywords:  # Игнорируем ключевые слова
                     self.setFormat(start_index, length, self.styles['variable'])
             
@@ -61,4 +84,4 @@ class PythonSyntaxHighlighter(QSyntaxHighlighter):
             else:
                 self.setFormat(start_index, length, self.styles['default'])
 
-            token = lexer.nextToken()
+            token = lexer.nextToken()  # Переходим к следующему токену
