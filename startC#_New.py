d7843a31b0fa52c767e987364c38490f66f97b97
Ratilife <ratilife81@gmail.com>
2025-02-01 18:15:41 +0300

    Изучение технологии ANTLR4

---
+++ startC#_New.py
@@ -0,0 +1,11 @@
+from antlr4 import *
+from CSharNew.CSharpLexer import CSharpLexer
+from CSharNew.CSharpParser  import CSharpParser 
+from CSharNew.CSharpPreprocessorParser  import CSharpPreprocessorParser 
+
+preprocessor_input = FileStream("C#Example.cs", encoding="utf-8")
+preprocessor_lexer = CSharpPreprocessorLexer(preprocessor_input)
+preprocessor_tokens = CommonTokenStream(preprocessor_lexer)
+preprocessor_parser = CSharpPreprocessorParser(preprocessor_tokens)
+preprocessed_tree = preprocessor_parser.compilation_unit()
+# ... дальше работа с preprocessed_tree (возможно, трансформация в другой поток токенов)
